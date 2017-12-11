8da76ade2f70e9c7f1c6bb656784542290a12d74
yves99 <60070104@kmitl.ac.th>
2017-12-11 18:04:01 +0700

    minor bug fixed

--- code/PlotGraph_internet.py
+++ code/PlotGraph_internet.py
@@ -3,53 +3,82 @@
 import pandas as pd
 import pygal
 
-def main(choice='1', year="all"):
-    """Core function"""
-    # input() = 53, 54 , 55, 56, 57, 58, 59, all
-    if choice == "user" or choice == "1":
-        return analytic(year, "/Taba.csv", "Number of Internet users by type", "Region", 1, 5)
-    elif choice == "location" or choice == "2":
-        return analytic(year, "/Tabb.csv", "Internet's using location", "Internet's using location", 2, 4)
-    elif choice == "access" or choice == "3":
-        return analytic(year, "/Tabc.csv", "Time period of Internet's access", "Region", 2, 9)
-    elif choice == "activity" or choice == "4":
-        return analytic(year, "/Tabd.csv", "Internet activities", "activity to use Internet", 2, 12)
+def main(choice='1', year='all'):
+    '''Core function
+        choice
+            1 = Number of Internet users by type of access
+            2 = Time period of Internet's access
+            3 = Internet activities
+            ########################
+        year = all, 53, 54, 55, 56, 57 ,58, 59
+    '''
+    data = call_data(choice, year)
+    if year == 'all' and choice == '3':
+        chart = pygal.Line()
+        chart.title = 'Total number of Internet users by Activities'
+        temp = []
+        data_add = [i for i in data[0]['Activity to use Internet '] if i != 'Total']
+        data_head = ['2553', '2554', '2555', '2556', '2557' , '2558', '2559']
 
-def analytic(year, path, name, x_axis_labels, start_select, stop_select):
-    """Analysis function"""
-    if year != "all":
-        data_frame = pd.read_csv("./usedata/" + year + path)
-        line_chart = pygal.Bar()
-        line_chart.title = name + " in " + year
-        line_chart.x_labels = list(data_frame[x_axis_labels])
-        column_list = list(data_frame.columns.values)[start_select:stop_select]
-        for column in column_list:
-            raw_data_list = list(data_frame[column])
-            total_list = list(data_frame["Total"])
-            percentage_list = list()
-            for i in range(len(raw_data_list)):
-                if total_list[i] == 0:
-                    total_list[i] = 1
-                percentage_list.append((raw_data_list[i] / total_list[i]) * 100)
-            line_chart.add(column, percentage_list)
-        return line_chart
-        
+        for i in range(1, len(data_add)+1):
+            info = [(data[j]['Internet using '][i]*100)//data[j]['Total'][0] \
+            for j in range(len(data_head))]
+            temp.append(info)
+
+        for i in range(len(data_add)):
+            chart.add(data_add[i], temp[i])
+
+    elif year == 'all':
+        chart = pygal.Line()
+        if choice == '1':
+        	chart.title = 'Total number of Internet users by Type'
+        else:
+        	chart.title = 'Total number of Internet users by Time period'
+        temp = []
+        memo = [i for i in data[0]]
+        data_head = ['2553', '2554', '2555', '2556', '2557' , '2558', '2559']
+        data_add = memo[2:]
+
+        for i in data_add:
+            info = [(data[j][i][0]*100)//data[j]['Total'][0]\
+            for j in range(len(data_head))]
+            temp.append(info)
+
+        for i in range(len(data_add)):
+            chart.add(data_add[i], temp[i])
+
     else:
-        df_list = []
-        total_list = []
-        for i in range(53, 60):
-            df_list.append(pd.read_csv("./usedata/"+ str(i) + path))
-        line_chart = pygal.Line()
-        line_chart.title = 'Total number of Internet users by ' + x_axis_labels
-        line_chart.x_labels = map(str, range(2553, 2560))
-        temp_list = []
-        for i in range(len(df_list[0][x_axis_labels])):
-            for j in df_list:
-                temp_list.append(j["Total"][i])
-            total_list.append(temp_list)
-            temp_list = []
-        for i in range(len(total_list)):
-            total_list[i] = list(map(lambda x: round((x/sum(total_list[i]))*100, 2), total_list[i]))
-        for i in range(len(df_list[0][x_axis_labels])):
-            line_chart.add(df_list[0][x_axis_labels][i], total_list[i])
-        return line_chart
+        chart = pygal.Bar()
+        memo = [i for i in data]
+        data_head = [i for i in data[memo[0]] if i != 'Whole kingdom' and i != 'Total']
+        chart.title = memo[0] + ' in 25' + year
+        
+        for i in range(2, len(memo)):
+            chart.add(str(memo[i]), [int((j*100)//data[memo[1]][0]) \
+            for j in data[memo[i]][1:]])
+
+    
+    chart.x_labels = data_head
+    chart.render_to_file('../Graph/Graph_internet.svg')
+
+
+def call_data(choice, year):
+    '''call data from csv'''
+    data = []
+    location = {'1':'../usedata/' + year + '/Taba.csv',
+                '2':'../usedata/' + year + '/Tabc.csv',
+                '3':'../usedata/' + year + '/Tabd.csv'}
+    all_year = ['53', '54', '55', '56', '57' , '58', '59']
+    if year == 'all':
+        for i in all_year:
+            location = {'1':'../usedata/' + i + '/Taba.csv',
+                        '2':'../usedata/' + i + '/Tabc.csv',
+                        '3':'../usedata/' + i + '/Tabd.csv'}
+            year = i
+            data.append(pd.read_csv(location[choice]))
+    else:
+        data = pd.read_csv(location[choice])
+
+    return data
+
+main(input(), input())
