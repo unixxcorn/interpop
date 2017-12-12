438da5eb2d049f2c3c0152e1edcc078d6cf58d63
pittawas17 <32727527+pittawas17@users.noreply.github.com>
2017-12-12 12:23:02 +0700

    Update PlotGraph_phone.py

--- code/PlotGraph_phone.py
+++ code/PlotGraph_phone.py
@@ -25,8 +25,6 @@ def main(year = 'all', area = 'whole', kind = 'all'):
         k_forf = 2
     elif kind.lower() == 'function': #function
         k_forf = 3
-    elif kind.lower() == 'other': #other
-        k_forf = 4
     return plot_graph(y_forf, a_forf, k_forf)
 
 def plot_graph(year, area, kind):
@@ -45,8 +43,6 @@ def plot_graph(year, area, kind):
         k_name = 'Mobile banking'
     elif kind == 3:
         k_name = 'Mobile function'
-    elif kind == 4:
-        k_name = 'Other'
     elif kind == 'all':
         k_name = 'ทั้งหมด'
     data_frame = pd.read_csv('./usedata/alldata.csv')
@@ -72,10 +68,6 @@ def plot_graph(year, area, kind):
                                    (data_frame['55u'])[3+a_start]*100/(data_frame['55t'])[3+a_start], (data_frame['56u'])[3+a_start]*100/(data_frame['56t'])[3+a_start], \
                                    (data_frame['57u'])[3+a_start]*100/(data_frame['57t'])[3+a_start], (data_frame['58u'])[3+a_start]*100/(data_frame['58t'])[3+a_start], \
                                    (data_frame['59u'])[3+a_start]*100/(data_frame['59t'])[3+a_start]])
-            line_chart.add('Other', [(data_frame['53u'])[4+a_start]*100/(data_frame['53t'])[4+a_start], (data_frame['54u'])[4+a_start]*100/(data_frame['54t'])[4+a_start], \
-                                   (data_frame['55u'])[4+a_start]*100/(data_frame['55t'])[4+a_start], (data_frame['56u'])[4+a_start]*100/(data_frame['56t'])[4+a_start], \
-                                   (data_frame['57u'])[4+a_start]*100/(data_frame['57t'])[4+a_start], (data_frame['58u'])[4+a_start]*100/(data_frame['58t'])[4+a_start], \
-                                   (data_frame['59u'])[4+a_start]*100/(data_frame['59t'])[4+a_start]])
         elif kind != 'all':
             line_chart.add(k_name, [(data_frame['53u'])[kind+a_start]*100/(data_frame['53t'])[kind+a_start], (data_frame['54u'])[kind+a_start]*100/(data_frame['54t'])[kind+a_start], \
                                    (data_frame['55u'])[kind+a_start]*100/(data_frame['55t'])[kind+a_start], (data_frame['56u'])[kind+a_start]*100/(data_frame['56t'])[kind+a_start], \
@@ -83,9 +75,9 @@ def plot_graph(year, area, kind):
                                    (data_frame['59u'])[kind+a_start]*100/(data_frame['59t'])[kind+a_start]])
     elif year != '53-2559':
         if kind == 'all':
-            line_chart.x_labels = ('SMS', 'Data', 'Mobile banking', 'Mobile function', 'Other')
+            line_chart.x_labels = ('SMS', 'Data', 'Mobile banking', 'Mobile function')
             lst_data = []
-            for i in range(5):
+            for i in range(4):
                 lst_data.append((data_frame[year+'u'])[i+a_start]*100/data_frame[year+'t'][i+a_start])
             line_chart.add('ผู้ใช้บริการปี '+year, lst_data)
         elif kind != 'all':
